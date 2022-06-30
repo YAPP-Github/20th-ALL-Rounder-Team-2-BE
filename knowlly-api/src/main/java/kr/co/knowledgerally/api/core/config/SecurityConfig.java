4ebaee45d3ba1d30512d6f0885cfa88c4907a025
hkpark02 <hkpark02@cafe24corp.com>
2022-06-30 23:06:17 +0900

    Fix: cors 이슈 해결

--- knowlly-api/src/main/java/kr/co/knowledgerally/api/core/config/SecurityConfig.java
+++ knowlly-api/src/main/java/kr/co/knowledgerally/api/core/config/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .exceptionHandling()
                 .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .and().cors()
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
