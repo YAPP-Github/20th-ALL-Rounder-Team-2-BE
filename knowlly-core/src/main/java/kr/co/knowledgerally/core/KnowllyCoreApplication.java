70be7a49f241148b10f1e687b9369f6ed4de1fd9
hkpark02 <hkpark02@cafe24corp.com>
2022-07-07 03:06:24 +0900

    Fix: Timezone 관련 설정 삭제

--- knowlly-core/src/main/java/kr/co/knowledgerally/core/KnowllyCoreApplication.java
+++ knowlly-core/src/main/java/kr/co/knowledgerally/core/KnowllyCoreApplication.java
@@ -9,11 +9,6 @@ import java.util.TimeZone;
 @SpringBootApplication
 public class KnowllyCoreApplication {
 
-	@PostConstruct
-	public void initialize() {
-		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(KnowllyCoreApplication.class, args);
 	}
