75aa7aeda7c7e201281c485954c5ff6c0c55b691
hkpark02 <hkpark02@cafe24corp.com>
2022-07-09 00:19:28 +0900

    Update: 운영 클래스 조회 신청자들 profileimage 추가 (#126) Fix: Mapper 수정 (#127)

--- knowlly-api/src/main/java/kr/co/knowledgerally/api/lecture/component/CoachLectureMapper.java
+++ knowlly-api/src/main/java/kr/co/knowledgerally/api/lecture/component/CoachLectureMapper.java
@@ -1,10 +1,12 @@
 package kr.co.knowledgerally.api.lecture.component;
 
 import kr.co.knowledgerally.api.lecture.dto.CoachLectureDto;
+import kr.co.knowledgerally.api.user.component.UserMapper;
 import kr.co.knowledgerally.core.lecture.entity.Lecture;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -14,13 +16,16 @@ import java.time.format.DateTimeFormatter;
         unmappedTargetPolicy = ReportingPolicy.IGNORE,
         uses = {LectureMapper.class, LectureInformationMapper.class, FormMapper.class}
 )
-public interface CoachLectureMapper {
+public abstract class CoachLectureMapper {
+    @Autowired
+    protected UserMapper userMapper;
+
     @Mapping(target = "lecture", source = "lecture")
     @Mapping(target = "lectureInformation", source = "lecture.lectureInformation")
     @Mapping(target = "forms", source = "lecture.forms")
     @Mapping(target = "isMatched", expression = "java(lecture.getState() != " +
             "kr.co.knowledgerally.core.lecture.entity.Lecture.State.ON_BOARD)")
     @Mapping(target = "matchedUser", expression = "java(lecture.getState() != " + // forms에는 매칭된 사람 신청서만 있다는 전제조건을 깔고 감
-            "kr.co.knowledgerally.core.lecture.entity.Lecture.State.ON_BOARD ? lecture.getForms().get(0).getUser() : null)")
-    CoachLectureDto toDto(Lecture lecture);
+            "kr.co.knowledgerally.core.lecture.entity.Lecture.State.ON_BOARD ? userMapper.toDto(lecture.getForms().get(0).getUser()) : null)")
+    public abstract CoachLectureDto toDto(Lecture lecture);
 }
--- knowlly-api/src/main/java/kr/co/knowledgerally/api/lecture/component/FormMapper.java
+++ knowlly-api/src/main/java/kr/co/knowledgerally/api/lecture/component/FormMapper.java
@@ -2,12 +2,15 @@ package kr.co.knowledgerally.api.lecture.component;
 
 import kr.co.knowledgerally.api.lecture.dto.FormDto;
 import kr.co.knowledgerally.api.lecture.dto.LectureDto;
+import kr.co.knowledgerally.api.user.component.UserImageMapper;
 import kr.co.knowledgerally.api.user.component.UserMapper;
 import kr.co.knowledgerally.core.lecture.entity.Form;
 import kr.co.knowledgerally.core.lecture.entity.Lecture;
+import kr.co.knowledgerally.core.user.service.UserImageService;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -17,8 +20,15 @@ import java.time.format.DateTimeFormatter;
         unmappedTargetPolicy = ReportingPolicy.IGNORE,
         uses = { UserMapper.class, LectureMapper.class }
 )
-public interface FormMapper {
+public abstract class FormMapper {
+    @Autowired
+    protected UserImageMapper userImageMapper;
+
+    @Autowired
+    protected UserImageService userImageService;
+
     @Mapping(target = "expirationDate", expression = "java(form.getExpirationDate().toString())")
-    FormDto.ReadOnly toDto(Form form);
-    Form toEntity(FormDto lectureDto);
+    @Mapping(target = "userImage", expression = "java(userImageMapper.toDto(userImageService.findByUser(form.getUser())))")
+    public abstract FormDto.ReadOnly toDto(Form form);
+    public abstract Form toEntity(FormDto lectureDto);
 }
