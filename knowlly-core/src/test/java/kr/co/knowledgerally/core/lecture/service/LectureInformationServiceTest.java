2b4d42e53ecf7b5bfd3b8f45cc658e1e9de8cdba
Young Hun Park <pyhpyh0670@gmail.com>
2022-07-11 23:52:49 +0900

    FIX: 클래스-info 등록시 user.coach를 true로 저장 (#140)

--- knowlly-core/src/test/java/kr/co/knowledgerally/core/lecture/service/LectureInformationServiceTest.java
+++ knowlly-core/src/test/java/kr/co/knowledgerally/core/lecture/service/LectureInformationServiceTest.java
@@ -11,6 +11,8 @@ import kr.co.knowledgerally.core.lecture.entity.LectureInformation;
 import kr.co.knowledgerally.core.lecture.entity.Tag;
 import kr.co.knowledgerally.core.lecture.util.TestCategoryEntityFactory;
 import kr.co.knowledgerally.core.lecture.util.TestLectureInformationEntityFactory;
+import kr.co.knowledgerally.core.user.entity.User;
+import kr.co.knowledgerally.core.user.util.TestUserEntityFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
@@ -41,6 +43,7 @@ public class LectureInformationServiceTest {
 
     TestLectureInformationEntityFactory testLectureInformationEntityFactory = new TestLectureInformationEntityFactory();
     TestCategoryEntityFactory testCategoryEntityFactory = new TestCategoryEntityFactory();
+    TestUserEntityFactory testUserEntityFactory = new TestUserEntityFactory();
 
     @Test
     void 클래스_info_목록_조회() {
@@ -126,5 +129,23 @@ public class LectureInformationServiceTest {
         lectureInformation.setTags(tagSet);
         lectureInformation.setCategory(Category.builder().name(Category.Name.LANGUAGE).build());
         lectureInformationService.saveLectureInformation(lectureInformation, lectureInformation.getCoach().getUser());
+    }
+
+    @Test
+//    @ExpectedDatabase(value = "classpath:dbunit/expected/crud/lecture_information_insert_test.xml",
+//            assertionMode = DatabaseAssertionMode.NON_STRICT)
+    void 가입후_첫_클래스_info와_클래스_태그_등록() {
+        Set<Tag> tagSet = new LinkedHashSet<>();
+
+        tagSet.add(Tag.builder().content("테스트 내용6").build());
+        tagSet.add(Tag.builder().content("테스트 내용7").build());
+
+        LectureInformation lectureInformation = testLectureInformationEntityFactory.createEntityWithoutCoach(6L, 2L,2);
+        lectureInformation.setTags(tagSet);
+        lectureInformation.setCategory(Category.builder().name(Category.Name.LANGUAGE).build());
+        User user = testUserEntityFactory.createEntity(2L);
+        assertFalse(user.isCoach());
+        LectureInformation newLectureInfo = lectureInformationService.saveLectureInformation(lectureInformation, user);
+        assertTrue(newLectureInfo.getCoach().getUser().isCoach());
     }
 }
--- knowlly-core/src/test/java/kr/co/knowledgerally/core/lecture/util/TestLectureInformationEntityFactory.java
+++ knowlly-core/src/test/java/kr/co/knowledgerally/core/lecture/util/TestLectureInformationEntityFactory.java
@@ -1,5 +1,6 @@
 package kr.co.knowledgerally.core.lecture.util;
 
+import kr.co.knowledgerally.core.coach.entity.Coach;
 import kr.co.knowledgerally.core.core.util.TestEntityFactory;
 import kr.co.knowledgerally.core.lecture.entity.*;
 import kr.co.knowledgerally.core.coach.util.TestCoachEntityFactory;
@@ -61,6 +62,44 @@ public class TestLectureInformationEntityFactory implements TestEntityFactory<Le
         return LectureInformation.builder()
                 .id(entityId)
                 .coach(testCoachEntityFactory.createEntity(coachId, userId))
+                .category(testCategoryEntityFactory.createEntity(categoryId))
+                .lectureImages(lectureImages)
+                .tags(tags)
+                .lectures(lectures)
+                .topic(String.format("테스트%d 제목", entityId))
+                .introduce(String.format("안녕하세요. 테스트%d 입니다.", entityId))
+                .price(1)
+                .build();
+    }
+
+    public LectureInformation createEntityWithoutCoach(long entityId, long categoryId, long lectureImageNum) {
+        Set<LectureImage> lectureImages = new LinkedHashSet<>();
+        Set<Tag> tags = new LinkedHashSet<>();
+        List<Lecture> lectures = new ArrayList<>();
+
+        tags.add(Tag.builder()
+                .lectureInformation(LectureInformation.builder().build())
+                .build());
+
+        lectures.add(Lecture.builder()
+                .lectureInformation(LectureInformation.builder().build())
+                .startAt(LocalDateTime.of(2022, 6, 15, 10, 30, 50))
+                .endAt(LocalDateTime.of(2022, 6, 15, 11, 30, 50))
+                .build());
+
+        for (long index=1; index<=lectureImageNum; index++) {
+            lectureImages.add(
+                    LectureImage.builder()
+                            .id(index)
+                            .lectureInformation(LectureInformation.builder().build())
+                            .lectureImgUrl(String.format("http://lecture%d.img%d.url", entityId, index))
+                            .build()
+            );
+        }
+
+        return LectureInformation.builder()
+                .id(entityId)
+                .coach(Coach.builder().build())
                 .category(testCategoryEntityFactory.createEntity(categoryId))
                 .lectureImages(lectureImages)
                 .tags(tags)
